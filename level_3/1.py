8cd5c88459ccf3658edde1ef23927b13a8ad2dd5
abdulahmedov <abdulahmedov@gmail.com>
2023-11-24 20:47:04 +0300

    level_3 commit

--- level_3/1.py
+++ level_3/1.py
@@ -3,7 +3,7 @@ import decimal
 from constants import ___
 
 
-def get_transaction_amount(transaction_id: ___, transactions_amounts_map: ___) -> ___:
+def get_transaction_amount(transaction_id: int, transactions_amounts_map: typing.Mapping[int, decimal.Decimal]) -> decimal.Decimal | None:
     # попробуйте использовать typing.Mapping: transactions_amounts_map по смыслу не должен меняться внутри функции
     pass
 
